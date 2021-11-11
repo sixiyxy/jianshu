07b9674011b4a00287e7bb6cf110dd996c851878
sixiyxy <771702500@qq.com>
2021-11-11 14:57:05 +0800

    5-6 redux完成todolist删除

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -33,6 +33,14 @@ class TodoList extends Component {
         store.dispatch(action);
     }
 
+    handleItemDelete(index) {
+        const action = {
+            type: 'delete_todo_item',
+            index
+        }
+        store.dispatch(action);
+    }
+
     render() {
         return (
             <div style={{marginTop: '10px', marginLeft: '10px'}}>
@@ -49,9 +57,9 @@ class TodoList extends Component {
                     style = {{marginTop: '10px', width: '300px'}}
                     bordered
                     dataSource={this.state.list}
-                    renderItem={item => (
-                    <List.Item>
-                     {item}
+                    renderItem={(item, index) => (
+                    <List.Item onClick={this.handleItemDelete.bind(this. index)}>
+                        {item}
                     </List.Item>
                     )}
                 />
