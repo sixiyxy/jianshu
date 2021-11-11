126a343d15680147dbd6523577130a2d2faa750f
sixiyxy <771702500@qq.com>
2021-11-11 15:07:55 +0800

    actionType的拆分

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-import store from './store/'
+import store from './store/';
+import { CHANGE_INPUT_VALUE, DELETE_TODO_ITEM, ADD_TODO_ITEM } from './store/actionTypes';
 
 class TodoList extends Component {
 
@@ -20,7 +21,7 @@ class TodoList extends Component {
 
     handleInputChange(e) {
         const action = {
-            type: 'change_input_value',
+            type: CHANGE_INPUT_VALUE,
             value: e.target.value
         }
         store.dispatch(action);
@@ -28,14 +29,14 @@ class TodoList extends Component {
 
     handleBtnClick() {
         const action = {
-            type: 'add_todo_item'
+            type: ADD_TODO_ITEM
         };
         store.dispatch(action);
     }
 
     handleItemDelete(index) {
         const action = {
-            type: 'delete_todo_item',
+            type: DELETE_TODO_ITEM,
             index
         }
         store.dispatch(action);
@@ -67,5 +68,6 @@ class TodoList extends Component {
         )
     }
 }
+
 
 export default TodoList;
