ba8a97a4bd51d792dacddbe3c0256ec1d5bfc9f4
sixiyxy <771702500@qq.com>
2021-11-18 00:35:54 +0800

    8-4

--- jianshu/src/pages/home/component/Recommend.js
+++ jianshu/src/pages/home/component/Recommend.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { RecommendWrapper, RecommendItem } from '../style'
+import { connect } from 'react-redux'
 
 class Recommend extends Component  {
-    render() {
-       return (
-           <div>Recommend</div>
-       ) 
-    }
+  render() {
+    return (
+      <RecommendWrapper>
+        {
+          this.props.list.map((item) => {
+            return <RecommendItem imgUrl={item.get('imgUrl')} key={item.get('id')}/>
+          })
+        }
+      </RecommendWrapper>
+    ) 
+  }
 }
+const mapState = (state) => ({
+  list: state.getIn(['home','recommendList'])
+})
 
-export default  Recommend;
+export default connect( mapState, null )(Recommend);
--- jianshu/src/pages/home/style.js
+++ jianshu/src/pages/home/style.js
@@ -1,9 +1,50 @@
 import styled from 'styled-components'
 
+export const RecommendWrapper = styled.div`
+	margin: 30px 0;
+	width: 280px;
+`;
+
+export const RecommendItem = styled.div`
+	width: 280px;
+	height: 50px;
+	background: url(${(props) => props.imgUrl});
+	background-size: contain;
+`;
+
 export const HomeWrapper = styled.div`
 	overflow: hidden;
 	width: 960px;
 	margin: 0 auto;
+`;
+
+export const ListItem = styled.div`
+	overflow: hidden;
+	padding: 20px 0;
+	border-bottom: 1px solid #dcdcdc;
+	.pic {
+		display: block;
+		width: 125px;
+		height: 100px;
+		float: right;
+		border-radius: 10px;
+	}
+`;
+
+export const ListInfo =	styled.div`
+	width: 500px;
+	float: left;
+	.title {
+		line-height: 27px;
+		font-size: 18px;
+		font-weight: bold;
+		color: #333;
+	}
+	.desc {
+		line-height: 24px;
+		font-size: 13px;
+		color: #999;
+	}
 `;
 
 export const HomeLeft = styled.div`
